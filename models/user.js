7634373dd824e7a7bb09480049a80ef3ab5db7ae
JinethRZ <jineth.rodriguezz@gmail.com>
2020-12-11 08:56:50 -0500

    Clean code: user.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const userSchema = new mongoose.Schema({ 
     firstName: {type: String,required: true},
     lastName: {type: String,required: true},
@@ -9,5 +8,4 @@ const userSchema = new mongoose.Schema({
     role: { type: String, required: true },
     tipo: { type: String, required: true }
 })
-
 module.exports= mongoose.model('User', userSchema)
