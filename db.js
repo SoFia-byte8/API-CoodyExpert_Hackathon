a86e201e4c3f196501f5dca90ca4275a65d2507c
dcalderonalegre@gmail.com <dcalderonalegre@gmail.com>
2020-12-11 08:45:38 -0500

    Conexiones con Mongo y Heroku

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose');
 const config = require('./config')
 const conectDB = ()=>{
    
-    mongoose.connect('mongodb+srv://CoodyExpert-Morse:CoodyExpert-Morse@coodyexpert-morse.1aobr.mongodb.net/CoodyExpert?retryWrites=true&w=majority',{
+    mongoose.connect(config.mongoDB,{
         useNewUrlParser:true,useUnifiedTopology:true},(error)=>{
             if(error){
                 console.log('Error',error)
